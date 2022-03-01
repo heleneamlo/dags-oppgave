e353ed34879eeb06a06c91ada40496c606001156
Henning Amlo <72279725+henningamlo@users.noreply.github.com>
2022-03-01 10:14:57 +0100

    added boilerplate things

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express")
+
+
+
+const app = express();
+
+
+
+app.get("/", function (req, res) {
+
+    res.sendFile(__dirname + "/index.html")
+
+});
+
+
+
+app.listen(process.env.PORT || 3000, function () {
+
+    console.log("Server is running at port 3000");
+
+});
