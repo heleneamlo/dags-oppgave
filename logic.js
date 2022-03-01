eb59727d333baa8531e3ad0ae7de0e66c6049dd7
Henning Amlo <72279725+henningamlo@users.noreply.github.com>
2022-03-01 13:13:48 +0100

    added procfile

--- logic.js
+++ logic.js
@@ -1,12 +1,14 @@
 
 // Initialize Firebase
 const db = firebase.firestore();
-
-const form = document.querySelector('form');
+let rediger = document.getElementById("rediger");
+const form = document.querySelector('#nameForm');
 
 // Add a new document in collection "names"
-form.addEventlistener("submit", function(){
+form.addEventListener("submit", function(e){
+    e.preventDefault();
     let navn = document.getElementById("navn").value;
+    rediger.innerHTML = "Velkommen, vi ønsker deg alt som er godt, "+ navn +"!";
 db.collection("names").doc().set({
     name: navn
 })
@@ -18,11 +20,27 @@ db.collection("names").doc().set({
 });
 });
 
+let randomNum = Math.round(Math.random()*100);
+document.getElementById("lykketall").innerHTML = randomNum;
+
+let lastMin = new Date().getMinutes();
+let thisMin;
+setInterval(function(){
+    console.log("kjører")
+    thisMin = new Date().getMinutes();
+    if (thisMin !== lastMin){
+        lastMin = thisMin;
+        randomNum = Math.round(Math.random()*100);
+        document.getElementById("lykketall").innerHTML = randomNum;
+    }
+},1000);
+
 
 db.collection('names').onSnapshot(snapshot => {
     const docs = snapshot.docChanges()
     docs.forEach(doc => {
         if (doc.type == "added") {
+            let list = document.getElementById("list");
             const docdata = doc.doc.data()
             const fornavn = docdata.name
             list.innerHTML += `<li>${fornavn}</li>`;
