8fbe4ef14b0138a210d9be101cc12757612212fe
Henning Amlo <72279725+henningamlo@users.noreply.github.com>
2022-03-01 11:57:29 +0100

    firestore shit

--- logic.js
+++ logic.js
@@ -1,21 +1,5 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "./node_modules/firebase/app";
-import { getFirestore } from "./node_modules/firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyB9CMCdQHYzTc2sOqlbxlrRdiXwnPGl1ak",
-  authDomain: "dagsoppgave-69c62.firebaseapp.com",
-  projectId: "dagsoppgave-69c62",
-  storageBucket: "dagsoppgave-69c62.appspot.com",
-  messagingSenderId: "935042062511",
-  appId: "1:935042062511:web:8d2995fa889b9a76949c5a"
-};
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
 const db = getFirestore();
 
 const form = document.querySelector('form');
